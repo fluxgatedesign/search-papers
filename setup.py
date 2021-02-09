dfba7978c520ce186e1dd6469f9a27f3d378d3fc
fluxgatedesign <76801006+fluxgatedesign@users.noreply.github.com>
2021-02-09 16:38:27 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='papers_search',
-    version='1.0',
-    author='Shivansh Verma',
-    author_email='shivansh.research@gmail.com',
-    packages=find_packages(),
-)
